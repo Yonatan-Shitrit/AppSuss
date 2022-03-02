849ee6e10d6656f4025b5000bb84c80d77b86e86
Jonathan <yonatan5789@gmail.com>
2022-03-02 21:18:00 +0200

    update

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,12 +1,14 @@
 export default {
     template: `
         <header class="app-header">
+            <div class="header-container">
                 <h3>AppSuss</h3>
                 <nav class="head-nav">
                     <router-link to="/">Home |</router-link>
                     <router-link to="/mail"> Mail |</router-link>
                     <router-link to="/keep"> Keep</router-link>
                 </nav>
+            </div>
         </header>
     
     `
