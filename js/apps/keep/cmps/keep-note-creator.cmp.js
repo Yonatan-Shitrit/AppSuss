439950fa01146ffc2eb83d72adb5873aa3bc6248
Jonathan <yonatan5789@gmail.com>
2022-03-03 20:55:56 +0200

    yes

--- js/apps/keep/cmps/keep-note-creator.cmp.js
+++ js/apps/keep/cmps/keep-note-creator.cmp.js
@@ -9,16 +9,16 @@ export default {
             <div>
             <input type="text" :placeholder=inputPlaceholder v-model="newNoteInput">
             <input type="text" placeholder="Note Title" v-model="noteTitle">
-            <button @click="setNoteType('noteTxt')">
-                <img src="../../../img/keep/icons/font.png" alt="">
+            <button :style="txtSelected" @click="setNoteType('noteTxt')">
+                <img src="../../../img/keep/icons/text.png" alt="">
             </button>
-            <button @click="setNoteType('noteImg')">
+            <button :style="imgSelected" @click="setNoteType('noteImg')">
                 <img src="../../../img/keep/icons/image.png" alt="">
             </button>
-            <button @click="setNoteType('noteList')">
+            <button :style="listSelected" @click="setNoteType('noteList')">
                 <img src="../../../img/keep/icons/list.png" alt="">
             </button>
-            <button @click="setNoteType('noteVideo')">
+            <button :style="videoSelected" @click="setNoteType('noteVideo')">
                 <img src="../../../img/keep/icons/video.png" alt="">
             </button>
             <button @click="addNewNote">
@@ -40,7 +40,7 @@ export default {
             newNote.info.title = this.noteTitle;
             if (this.noteType === 'noteTxt') newNote.info.txt = this.newNoteInput;
             else if (this.noteType === 'noteImg' || this.noteType === 'noteVideo') newNote.info.url = this.newNoteInput;
-            else if (this.noteType === 'noteList') newNote.info.todos = this.newNoteInput.split(',').map(todo=> {return {txt: todo, done: false}});
+            else if (this.noteType === 'noteList') newNote.info.todos = this.newNoteInput.split(',').map(todo => { return { txt: todo, done: false } });
             keepService.save(newNote).then((note) => {
                 this.$emit('noteAdded', note);
                 this.newNoteInput = '';
@@ -57,6 +57,22 @@ export default {
             else if (this.noteType === 'noteImg') return 'Enter Image URL';
             else if (this.noteType === 'noteList') return `Enter list devided by ',' `;
             else if (this.noteType === 'noteVideo') return 'Enter Video URL';
-        }
+        },
+        txtSelected() {
+            if (this.noteType === 'noteTxt') return {'background-color': 'rgb(235, 235, 235)'};
+            else return '';
+        },
+        imgSelected() {
+            if (this.noteType === 'noteImg') return  {'background-color': 'rgb(235, 235, 235)'};
+            else return '';
+        },
+        listSelected() {
+            if (this.noteType === 'noteList') return  {'background-color': 'rgb(235, 235, 235)'};
+            else return '';
+        },
+        videoSelected() {
+            if (this.noteType === 'noteVideo') return  {'background-color': 'rgb(235, 235, 235)'};
+            else return '';
+        },
     }
 }
